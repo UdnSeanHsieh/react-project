94d8c3219b788d98d4247efb7f7e39f864e2d28e
nick12003 <34929382+nick12003@users.noreply.github.com>
2021-06-19 22:26:44 +0800

    改成function component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,104 @@
-
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import sort from 'array-sort';
+import { List, Form, Input, DatePicker, Button } from 'antd';
+import 'antd/dist/antd.css';
+import { CheckOutlined, CloseCircleFilled } from '@ant-design/icons';
 import './App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "",
-            list: []
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.addList = this.addList.bind(this);
-        this.onCompleted = this.onCompleted.bind(this);
-        this.onDelete = this.onDelete.bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
-    addList() {
-        this.setState(({ list }) => {
-            return {
-                value: "",
-                list: [...list, { title: this.state.value, completed: false }]
-            }
-        })
-    }
-
-    onCompleted(index) {
-        let list = [...this.state.list];
-        list[index].completed = !list[index].completed;
-        this.setState({ list: list });
-    }
-
-    onDelete(index) {
-        let list = [...this.state.list];
-        list.splice(index, 1);
-        this.setState({ list: list });
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="title">
-                    <h1>Todo list</h1>
-                </div>
-                <div className="list">
-                    {
-                        this.state.list.map(({ title, completed }, index) => {
-                            return <div key={index} className={`item ${completed ? "completed" : ""}`} onClick={() => {
-                                this.onCompleted(index);
-                            }}>
-                                <div className="item-check">V</div>
-                                <div className="item-title">{title}</div>
-                                <div className="item-del" onClick={(e) => {
-                                    e.stopPropagation();
-                                    this.onDelete(index)
-                                }}>X</div>
-                            </div>
-                        })
-                    }
-                </div>
-                <div className="input">
-                    <input type="text" value={this.state.value} placeholder="add a new todo..." onChange={this.handleChange} />
-                    <button onClick={this.addList}>Add</button>
+const TodoItem = ({
+    onDelete,
+    onCompleted,
+    id,
+    title,
+    completed,
+    date
+}) => {
+    return (
+        <List.Item
+            key={id}
+            actions={[
+                <CloseCircleFilled
+                    className="delete"
+                    onClick={() => {
+                        onDelete(id);
+                    }} />
+            ]}
+        >
+            <div className={`item ${completed ? "completed" : ""}`}>
+                <CheckOutlined onClick={() => {
+                    onCompleted(id);
+                }} />
+                <div>
+                    <div className="item-date">{date}</div>
+                    <div className="item-title">{title}</div>
                 </div>
             </div>
-        )
+        </List.Item>
+    )
+}
+
+const App = () => {
+    const [form] = Form.useForm();
+    const [list, setList] = useState([]);
+    const addList = ({ date, task }) => {
+        setList(list => sort([...list, { id: uuidv4(), title: task, completed: false, date: date.format('YYYY-MM-DD') }], ['date']));
+        form.resetFields();
     }
+    const onCompleted = (id) => {
+        let newlist = [...list];
+        let index = newlist.findIndex(e => e.id === id);
+        newlist[index].completed = !newlist[index].completed;
+        setList(newlist);
+    }
+    const onDelete = (id) => {
+        let newlist = [...list];
+        newlist.splice(newlist.findIndex(e => e.id === id), 1);
+        setList(newlist);
+    }
+    return (
+        <div className="container">
+            <div className="title">
+                <h1>
+                    Todo list
+                </h1>
+            </div>
+            {
+                list.length && <List
+                    size="large"
+                    className="list"
+                    dataSource={list}
+                    renderItem={item => (
+                        <TodoItem onDelete={onDelete} onCompleted={onCompleted} {...item} />
+                    )}
+                />
+            }
+            <Form
+                form={form}
+                className="form"
+                layout="inline"
+                onFinish={addList}
+            >
+                <Form.Item
+                    name="task"
+                    rules={[{ required: true, message: "填寫任務名稱" }]}
+                >
+                    <Input placeholder="addd a new todo..." />
+                </Form.Item>
+                <Form.Item
+                    name="date"
+                    rules={[{ required: true, message: "填寫日期" }]}
+                >
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Add
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    )
 }
 
 export default App;
